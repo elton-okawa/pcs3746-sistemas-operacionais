1022232d66f3ca16bcdcdbf94427a9eaccbb4282
elton-okawa <elton_okawa@hotmail.com>
2019-05-16 21:39:01 -0300

    feat: syscall save and recover

---
+++ 1/initramfs/save_recover.h
@@ -0,0 +1,14 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#define __NR_save 404
+#define __NR_recover 405
+
+int save(int number) {
+	return syscall(__NR_save, number);
+}
+
+int recover(void) {
+	return syscall(__NR_recover);
+}
