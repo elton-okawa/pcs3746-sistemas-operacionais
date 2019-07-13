8cb0e1329c7f04d4cc7e21cce5c6e1ad8de23265
elton-okawa <elton_okawa@hotmail.com>
2019-07-13 09:26:49 -0300

    chore: testing different array sizes

--- 4/src/fork_bomb.c
+++ 4/src/fork_bomb.c
@@ -4,11 +4,16 @@
 
 #include "kernel_malloc.h"
 
-
+// int array[10000000]; //funciona
+// int array[5050000]; //funciona
+// int array[6050000]; //funciona 
+// int array[6150000];  // funciona
+// int array[6200000];
+// int array3[15000000];
 int main() {
-  (double *)malloc(sizeof(double) * 16000000);
-  kernel_malloc(50000000L);
-  
+  // (double *)malloc(sizeof(double) * 16000000);
+  // kernel_malloc(50000000L);
+  printf("while\n");
   while(1) {
     fork();
   }
